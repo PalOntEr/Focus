84f0315e4124e2b401d319f91b43af33f409e2cf
Roberto Carlos <roberto.dominguezesp@uanl.edu.mx>
2024-09-17 03:01:12 -0600

    Updated home and course colours to correctly use the colour palette

--- views/home.view.php
+++ views/home.view.php
@@ -3,9 +3,9 @@
     require 'views/components/navbar.php';
 ?>
 <div class="container mx-auto">
-    <h1 class="text-8xl text-secondary text-center font-bold">Focus</h1>
-    <div class="w-full text-lg text-center mt-5">A better you, one course away</div>
-    <div class="w-full text-4xl font-semibold my-5">
+    <h1 class="text-8xl text-secondary text-center font-extrabold tracking-wide">FOCUS</h1>
+    <div class="w-full text-lg text-color font-semibold text-center mt-5">A better you, one course away</div>
+    <div class="w-full text-4xl text-secondary font-bold my-5">
         Best seller
     </div>
     <div class="w-full h-[500px] flex flex-col sm:flex-row overflow-y-hidden overflow-x-scroll">
