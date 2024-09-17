5d336b32b0a9b7e8b2515e275fad671be38016da
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2024-09-16 19:27:00 -0600

    Update page title and favicon in header.php

--- views/components/header.php
+++ views/components/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
+    <title>Focus</title>
+    <link rel="icon" href="https://static.wikia.nocookie.net/ultra-custom-night/images/f/f6/Purple_Freddy.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
         <?php require 'views/css/global.css'; ?>
