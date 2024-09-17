f6013048902eddb362abeedee07a38af4cb7e5ed
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2024-09-16 19:19:02 -0600

    Add header.php

---
+++ views/components/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        <?php require 'views/css/global.css'; ?>
+    </style>
+</head>
