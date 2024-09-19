c2e68cd691130d0c61930f26a602750f7cb0be72
Roberto Carlos <RCDE11@Outlook.com>
2024-09-19 11:44:54 -0600

    updated html structure of courseCard.php

--- views/components/courseCard.php
+++ views/components/courseCard.php
@@ -9,10 +9,6 @@
         <div class="text-comp-2">Instructor</div>
         <div class="text-comp-2">4.3/5⭐</div>
     </div>
-    <button onclick="addToCart()" class="w-1/2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
-        ADD TO CART
-    </button>
-</div>
     <div class="flex flex-row justify-evenly w-full">
         <button onclick="location.href='/course'" class="w-full mx-2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
         PREVIEW
@@ -21,6 +17,7 @@
             ADD TO CART
         </button>
     </div>
+</div>
 <script>
     function addToCart() {
         swal({
@@ -31,4 +28,4 @@
         });
     };
 </script>
-</div>
+
