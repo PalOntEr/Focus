32b5b3b3bd7df49b2f7ba346824555ba28717e35
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2024-09-18 23:59:48 -0600

    Imported Sweet Alert library

--- views/components/courseCard.php
+++ views/components/courseCard.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-5/6 sm:w-1/3 h-full max-h-[367px] bg-primary m-5 sm:m-2 p-5 justify-center items-center rounded-2xl shadow-lg">
+<div class="flex flex-col w-5/6 sm:w-1/3 bg-primary m-5 sm:m-2 p-5 justify-center items-center rounded-2xl shadow-lg hover:scale-50">
     <div class="flex justify-center w-full h-5/6 mb-5 bg-color rounded-2xl">
         <img class="w-auto h-auto rounded-2xl" src="https://pbs.twimg.com/media/GVq8fLsaoAEnzsl?format=jpg&name=large" alt="Course">
     </div>
@@ -9,7 +9,17 @@
         <div class="text-comp-2">Instructor</div>
         <div class="text-comp-2">4.3/5⭐</div>
     </div>
-    <button class="w-1/2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
+    <button onclick="addToCart()" class="w-1/2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
         ADD TO CART
     </button>
 </div>
+<script>
+    function addToCart() {
+        swal({
+            title: 'Success!',
+            text: 'The course has been added to your cart.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    };
+</script>
--- views/components/header.php
+++ views/components/header.php
@@ -6,6 +6,7 @@
     <title>Focus</title>
     <link rel="icon" href="https://static.wikia.nocookie.net/ultra-custom-night/images/f/f6/Purple_Freddy.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <style>
         <?php require 'views/css/global.css'; ?>
     </style>
