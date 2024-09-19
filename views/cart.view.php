04c0755062a0c90719a621947cede0e13001306d
Roberto Carlos <RCDE11@Outlook.com>
2024-09-18 19:27:45 -0600

    updated the cart view as well as the buy course Card component

--- views/cart.view.php
+++ views/cart.view.php
@@ -7,19 +7,15 @@
 ?>
 <div class="container flex flex-col mx-auto">
 <h1 class="text-8xl text-primary text-center font-extrabold tracking-wide">Carrito </h1>
-<div class="flex flex-row">
+<div class="flex flex-row my-5">
     <div class="w-1/4 flex flex-col">
         <div id="Cart-Container" class="h-auto bg-primary rounded-md p-4">
-            <div class="flex flex-row justify-between">
-            <div class="text-sm p-3 text-left text-color p-4">PHP Course.-$550 MXN.</div>
-            <button class="">Eliminar</button>
-        </div>
-        <div class="h-px w-full bg-comp-2"></div>
             <div>
-            <div class="font-bold text-comp-1 text-sm my-4">Total de Cursos: <span class="font-normal text-color">7</span></div>
-            <div class="font-bold text-comp-1 text-sm my-4">TOTAL: <span class="font-normal text-color">$480.00 MXN</span></div>
-            <div class="font-bold text-comp-1 text-sm my-4">TOTAL + IVA: <span class="font-normal text-color">$524.00 MXN</span></div>
-            <button class="w-full bg-comp-1 text-sm p-2 text-color rounded-md">Pagar.</button>
+                <div class="font-bold text-comp-1 text-sm my-3">Total de Cursos: <span class="font-normal text-color">7</span></div>
+                <div class="font-bold text-comp-1 text-sm my-3">SUBTOTAL: <span class="font-normal text-color">$524.00 MXN</span></div>
+                <div class="font-bold text-comp-1 text-sm my-3">TOTAL: <span class="font-normal text-color">$480.00 MXN</span></div>
+                <div class="h-px w-full bg-comp-2 my-4"></div>   
+                <button class="w-full bg-comp-1 text-sm p-2 text-color rounded-md">Pagar.</button>
             </div>
         </div>
     </div>
--- views/components/buycourseCard.php
+++ views/components/buycourseCard.php
@@ -1,5 +1,5 @@
-<div class="flex flex-row w-full px-4 h-2/3 bg-primary m-5 sm:m-2 items-center rounded-2xl">
-    <div  class="w-2/3 h-5/6 bg-transparent rounded-2xl">
+<div class="flex flex-row w-full px-4 h-2/3 bg-primary mb-5 sm:mb-2 items-center rounded-2xl shadow-lg">
+    <div  class="w-2/3 h-5/6 my-2 bg-transparent rounded-2xl">
         <img class="w-auto h-auto rounded-2xl" src="https://pbs.twimg.com/media/GVq8fLsaoAEnzsl?format=jpg&name=large" alt="Course">
         <button class="text-center bg-comp-1 text-color w-full mt-2 rounded p-px hover:opacity-80 font-bold">
         SEE COURSE
@@ -7,16 +7,17 @@
     </div>
     <div class="w-full  h-60 ml-2 my-4">
     <div class="h-full justify-evenly flex flex-col">
-    <div class="w-full text-secondary font-semibold flex flex-col justify-between">
-        PHP Course 
-        <div class="text-comp-2">Instructor 4.3/5⭐</div>
+    <div class="w-full text-comp-2 font-semibold flex flex-col justify-between">
+        <div class="flex">PHP Course - 4.3/5⭐</div>
+        <div class="text-secondary w-full flex place-content-between"><p>Instructor</p></div>
     </div>
     <div class="flex w-full text-color justify-center text-sm h-full">
         Hola aqui iria la descripcion del curso jaja aqui seria la descripcion del curso jaja espero que les guste este curso fua chavales
         sjajaja que pedo jajajajajajajjajajjajajajajjajajajaaj asdasjpdajdfsndlsnd no te duermas chema jaja esto es un test de como se va a
         ver la descripcion del chats
     </div>
-    <div class="w-full flex flex-row items-center justify-between">
+    <div class="w-full flex flex-row items-center justify-end">
+    <button class="">Eliminar</button>
     </div>
     </div>
     </div>
