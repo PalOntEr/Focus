b8731db64cdebfa9a5e0e816cc5c6b6f9092bb71
Roberto Carlos <roberto.dominguezesp@uanl.edu.mx>
2024-09-20 13:00:41 -0600

    updated the function add to cart

--- views/components/courseCard.php
+++ views/components/courseCard.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col w-5/6 sm:w-1/3 bg-primary m-5 sm:m-2 p-5 justify-center items-center rounded-2xl shadow-lg hover:scale-50">
+<div class="flex flex-col w-5/6 md:w-1/3 bg-primary m-5 md:m-2 p-5 justify-center items-center rounded-2xl shadow-lg hover:scale-150">
     <div class="flex justify-center w-full h-5/6 mb-5 bg-color rounded-2xl">
         <img class="w-auto h-auto rounded-2xl" src="https://pbs.twimg.com/media/GVq8fLsaoAEnzsl?format=jpg&name=large" alt="Course">
     </div>
@@ -13,7 +13,7 @@
         <button onclick="location.href='/course'" class="w-full mx-2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
         PREVIEW
         </button>
-        <button class="w-full mx-2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
+        <button onclick="addToCart()" class="w-full mx-2 text-center bg-comp-1 text-color rounded p-1 hover:opacity-80 font-bold">
             ADD TO CART
         </button>
     </div>
@@ -21,9 +21,9 @@
 <script>
     function addToCart() {
         swal({
-            title: 'Success!',
-            text: 'The course has been added to your cart.',
             icon: 'success',
+            title: '🎉',
+            text: 'The course has been added to your cart.',
             confirmButtonText: 'OK'
         });
     };
