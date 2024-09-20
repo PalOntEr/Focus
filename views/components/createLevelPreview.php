ed5580cf8ebcff8693010e093f2369104b1328a4
Roberto Carlos <roberto.dominguezesp@uanl.edu.mx>
2024-09-20 12:58:44 -0600

    Updated the colours to better fit the colour palette

--- views/components/createLevelPreview.php
+++ views/components/createLevelPreview.php
@@ -4,18 +4,24 @@
             <?= $levelNum ?>
         </div>
         <div class="w-5/12">
-            <input name="levelName" class="w-full rounded-md p-1 bg-comp-2 text-comp-1" placeholder="Level Name" type="text">
+            <input name="levelName" class="w-full rounded-md p-1 bg-transparent outline-none text-color" placeholder="Level Name" type="text">
         </div>
         <div class="w-1/2">
-            <input name="levelDescription" class="w-full rounded-md p-1 bg-comp-2 text-comp-1" placeholder="Level Description" type="text">
+            <input name="levelDescription" class="w-full rounded-md p-1 bg-transparent outline-none text-color" placeholder="Level Description" type="text">
         </div>
     </div>
     <div class="flex items-center justify-end space-x-2 w-1/2">
         <a href="#" download>
-            <img class="h-5" src="https://cdn-icons-png.flaticon.com/512/12334/12334190.png" alt="">
+        <svg class="w-[48px] h-[48px] text-color" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 0 1-1 1H5m14-4v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1ZM9 12h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1Zm5.697 2.395v-.733l1.269-1.219v2.984l-1.268-1.032Z"/>
+        </svg>
         </a>
+
         <a href="#" download>
-            <img class="h-5" src="https://cdn-icons-png.flaticon.com/512/84/84539.png" alt="">
+<svg class="w-[48px] h-[48px] text-color" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h4M9 3v4a1 1 0 0 1-1 1H4m11 6v4m-2-2h4m3 0a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"/>
+</svg>
+
         </a>
         <div>
             <label class="flex items-center space-x-1">
@@ -26,7 +32,9 @@
         <div>
             <label class="flex items-center space-x-1">
                 <a href="#">
-                    <img class="h-5" src="https://cdn-icons-png.flaticon.com/512/1017/1017530.png" alt="Remove">
+                <svg class="w-[48px] h-[48px] text-color" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                </svg>
                 </a>
             </label>
         </div>
