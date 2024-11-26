4f0ad187bb0cb0e4cf1bd13a5abe57422e70e2f2
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2024-11-25 18:29:27 -0600

    Created CategoryInfo view and added it into the sp_Categories

---
+++ models/queries/vw_CategoryInfo.sql
@@ -0,0 +1,5 @@
+DROP VIEW IF EXISTS CategoryInfo;
+
+CREATE VIEW CategoryInfo AS
+SELECT `categoryName`,`categoryDescription`,u.`fullName`, c.`creationDate`
+ FROM categories c JOIN users u ON c.`creatorId` = U.`userId`
