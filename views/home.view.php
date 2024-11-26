2653eec3bb5f309c4e80b265ce113f51c141d3b3
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2024-11-26 14:02:33 -0600

    added fetch of categories and implemented the fetch into front end

--- views/home.view.php
+++ views/home.view.php
@@ -19,25 +19,47 @@
         Categories
     </div>
     <div class="h-1 bg-secondary"></div>
-    <div class="w-full h-1/2 py-5 flex flex-col justify-center items-center space-x-0 md:space-x-12 md:flex-row overflow-y-hidden overflow-x-auto">
-    
-    <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
-        <a href="/advSearch">
-        <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Computer Science</h2>
-        <p class="text-center text-primary text-lg font-semibold">Computer related stuff</p>
+    <div id="categories" class="w-full h-1/2 py-5 flex flex-col justify-center items-center space-x-0 md:space-x-12 md:flex-row overflow-y-hidden overflow-x-auto">
+        <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
+            <a href="/advSearch">
+            <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Computer Science</h2>
+            <p class="text-center text-primary text-lg font-semibold">Computer related stuff</p>
+            </a>
+        </div>
+        <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
+            <a href="/advSearch">
+            <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Languages</h2>
+            <p class="text-center text-primary text-lg font-semibold">Languages related stuff</p>
+            </a>
+        </div>
+        <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
+        <a href="/advSearch">    
+        <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Engineering</h2>
+            <p class="text-center text-primary text-lg font-semibold">Engineering related stuff</p>
         </a>
-    </div>
-    <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
-        <a href="/advSearch">
-        <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Languages</h2>
-        <p class="text-center text-primary text-lg font-semibold">Languages related stuff</p>
-        </a>
-    </div>
-    <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
-    <a href="/advSearch">    
-    <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">Engineering</h2>
-        <p class="text-center text-primary text-lg font-semibold">Engineering related stuff</p>
-    </a>
-    </div>
+        </div>
     </div>
 </div>
+
+<script>
+fetch("/categories")
+.then(response => response.json())
+.then(data => {
+    const categoriesContainer = document.getElementById("categories");
+
+    data.payload.categories.forEach((category) => {
+        categoriesContainer.innerHTML = data.payload.categories
+            .map(
+                (category) => `
+                <div class="flex-col justify-center w-1/2 h-1/2 md:w-1/5 md:h-1/5 mb-5 bg-color rounded-2xl">
+                    <a href="/advSearch?category=${category.categoryId}">
+                        <h2 class="text-center font-extrabold text-secondary text-4xl mb-3">${category.categoryName}</h2>
+                        <p class="text-center text-primary text-lg font-semibold">${category.categoryDescription}</p>
+                    </a>
+                </div>
+                `
+            )
+            .join(""); // Join the array of strings into a single HTML string
+    });
+});
+</script>
