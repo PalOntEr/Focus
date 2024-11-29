621d990dff18d74d453c238cac88528d6066c1b5
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2024-11-29 17:20:54 -0600

    Added specific level functionality
    To Do: implement download resource file

--- views/level.view.php
+++ views/level.view.php
@@ -12,19 +12,17 @@
             Level name
         </div>
         <div class="bg-gray-600 m-4 rounded-xl">
-            <video class="rounded-xl" src="https://videos.pexels.com/video-files/855390/855390-uhd_2560_1440_25fps.mp4" autoplay loop></video>
+            <video id="Levelvideo" controls class="rounded-xl" src="https://videos.pexels.com/video-files/855390/855390-uhd_2560_1440_25fps.mp4" autoplay loop></video>
         </div>
+
         <div class="text-xl text-secondary">
             Resources
-            <div class="bg-primary space-y-2 rounded-md p-2">
-                <?php                
-                    for( $i = 1; $i <= 5; $i++ ) {
-                        $docName = "Document $i";
-                        $docRef = "https://www.google.com";
-                        require 'views/components/resource.php';
-                    }
-                ?>
+            <div id="ResourcesContainer" class="bg-primary space-y-2 rounded-md p-2">
             </div>
+        </div>
+
+        <div class="mt-3">
+        <button id="FinishLevel" class="w-full p-2 bg-primary rounded-md text-color">Finish Level</button>
         </div>
     </div>
 
@@ -32,17 +30,7 @@
         <div class="mt-4 text-center text-comp-2 font-bold text-3xl">
             Content
         </div>
-        <div class="mx-4">
-            <?php
-                for( $i = 1; $i <= 100; $i++ ) {
-                    $level = $i;
-                    $levelComplete = false;
-                    if(rand(0,1) == 1){
-                        $levelComplete = true;
-                    }
-                    require 'views/components/levelPreview.php';
-                }
-            ?>
+        <div id="LevelsContainer" class="mx-4">
         </div>
     </div>
 </div>
@@ -56,8 +44,9 @@ document.addEventListener("DOMContentLoaded", async ()=>{
     usersData.payload.users.forEach(user => {
         users[user.userId] = user;
     });
-
+    
     let CourseId = <?= $_GET['course'] ?? 1 ?>;
+    let LevelsFound = {}; 
 
     function getCourseInformation(){
         fetch("/courses/get?course_id=" + CourseId)
@@ -66,34 +55,100 @@ document.addEventListener("DOMContentLoaded", async ()=>{
             document.getElementById("CourseName").textContent = data.payload.courses[0].courseTitle;
         });
     }
-
     function getLevelsInformation(){
+        let PurchasedLevels = {};        
+        fetch("/purchasedLevels/get?user_Id=" + <?= json_encode($_SESSION['user']['userId']) ?>)
+        .then(response => response.json())
+        .then(data => {
+            PurchasedLevels = data.payload.PurchasedLevels;
+        });
+
         fetch("/level?course_id=" + CourseId)
         .then(response => response.json())
         .then(data => {
-            const LevelsFound = data.payload.levels;
+            LevelsFound = data.payload.levels;
 
             LevelsFound.forEach(levelFound => {
-                
+                    const levelPreviewContainer = document.getElementById("LevelsContainer");
+                    const levelPreview = document.createElement('div');
+                    let levelhtml = `<?php require 'views/components/levelPreview.php'; ?>`;
+                    levelhtml = levelhtml.replace(/LEVEL_ID/g,levelFound.levelId);
+                    levelhtml = levelhtml.replace(/LEVEL_NUM/g,levelFound.levelNumber);
+                    levelhtml = levelhtml.replace(/COURSE_ID/g,levelFound.courseId);
+                    levelPreview.innerHTML = levelhtml;
+
+                    levelPreviewContainer.append(levelPreview);
+                    
+                    levelPreview.querySelector(".Level").onclick = function () {
+                        alert("No lo tienes comprado gg");
+                    };
+
+                    const exists = PurchasedLevels.some(Purchasedlevel => Purchasedlevel.levelId === levelFound.levelId);
+                    if(exists)
+                    {
+                        levelPreview.querySelector(".Level").onclick = function () {
+                            location.href = '/levels?level=' + levelFound.levelId + '&course=' + levelFound.courseId;
+                            };
+                    }
+
+                    if (<?= $_GET["level"] ?> === levelFound.levelId) {
+                        const levelNameElement = document.getElementById("LevelName");
+                        if (levelNameElement) {
+                            levelNameElement.textContent = levelFound.levelName;
+                        }
+                    }
             });
         });
     }
 
     function getLevelInformation(){
-        fetch("/level?course_id="+ CourseId + "&level_id="+ <?= $_GET["level"];?> )
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("LevelName").textContent = data.payload.levels[0].levelName;
-            
             fetch("/Content/get?level_id="+<?= $_GET["level"];?>)
             .then(response => response.json())
             .then(data => {
-                
-            });
+                const ContentFound = data.payload.content;
+
+                ContentFound.forEach(Content => {
+                if(Content.mimeType === ".mp4")
+                {
+                    let Route = atob(Content.file);
+                    const decodedRoute = atob(Route);
+                    console.log(decodedRoute);
+                    const videoElement = document.getElementById("Levelvideo");
+                    videoElement.src = decodedRoute;
+                    videoElement.load();
+                    videoElement.play();
+                }
+                else{
+                    const levelPreviewContainer = document.getElementById("ResourcesContainer");
+                    const levelPreview = document.createElement('div');
+                    let levelhtml = `<?php require 'views/components/resource.php'; ?>`;
+                    levelhtml = levelhtml.replace(/RESOURCE_ID/g,ContentFound.contentId);
+                    levelPreview.innerHTML = levelhtml;
+
+                    levelPreviewContainer.append(levelPreview);
+                    document.getElementById("Resource").textContent = Content.name;
+                }
+                });
         });
     }
     getCourseInformation();
     getLevelInformation();
     getLevelsInformation();
+});
+
+document.getElementById("FinishLevel").addEventListener("click",function(){
+    let formData = new FormData();
+
+    formData.append("user_Id",<?= json_encode($_SESSION['user']['userId']) ?>);
+    formData.append("level_Id",<?= $_GET["level"];?>);
+    formData.append("completed",1);
+    
+    fetch("/purchasedLevels/put",{
+    method:"POST",
+    body:formData
+    }).then(response => response.json())
+    .then(data => console.log(data));
+    
+    
 });
 </script>
