555bcd0b6015f10beff2dd5d9b01141824df1cc2
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2024-11-29 17:21:23 -0600

    Added RESOURCE ID for dynamic ID of the div

--- views/components/resource.php
+++ views/components/resource.php
@@ -2,12 +2,14 @@
     <div class="flex items-center space-x-2">
         <img class="h-8 bg-color" src="https://cdn-icons-png.flaticon.com/512/80/80942.png" alt="">
         <div class="text-color">
-            <?= $docName ?>
         </div>
     </div>
+    <p id="Resource" class="text-color"></p>
     <div>
-        <a href="<?= $docRef ?>" download>
+        <a href="#" id="DownloadFile">
+            <div id="RESOURCE_ID">
             <img class="h-5 bg-color" src="https://cdn-icons-png.flaticon.com/512/3580/3580085.png" alt="">
+            </div>
         </a>
     </div>
 </div>
