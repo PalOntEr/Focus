47430056cabc63376b00f74ded01666ddf517e5e
Roberto Carlos <100060334+RobertoCarlos11@users.noreply.github.com>
2024-11-28 23:25:10 -0600

    minor refactor of levelCart to have placeHolders LEVEL_NUM

--- views/components/levelCard.php
+++ views/components/levelCard.php
@@ -1,12 +1,12 @@
-<div class="flex flex-col w-full sm:w-48 h-fit sm:h-48 max-h-[367px] bg-primary mb-2 sm:m-2 p-2 justify-center items-center rounded-2xl" style="width=500px;">
-    <div class="flex justify-center w-full h-2/3 mb-1 bg-color rounded-2xl">
-        <img class="w-auto h-auto rounded-2xl" src="https://static.wikia.nocookie.net/ultra-custom-night/images/f/f6/Purple_Freddy.png" alt="Level <?= $level ?>">
+<div id="LEVEL_NUM" class="flex flex-col w-full sm:w-48 h-fit sm:h-48 max-h-[367px] bg-primary mb-2 sm:m-2 p-2 justify-center items-center rounded-2xl" style="width=500px;">
+    <div class="flex justify-center w-1/2 h-2/3 mb-1 bg-color rounded-2xl">
+        <img class="CourseImage rounded-2xl" src="https://static.wikia.nocookie.net/ultra-custom-night/images/f/f6/Purple_Freddy.png" alt="Level <?= $level ?>">
     </div>
     <div class="flex w-full h-1/6 p-1 items-center justify-between font-semibold">
             <div class="w-full text-color font-semibold">
-                Level <?= $level ?>
+                Level LEVEL_NUM
             </div>
-            <div class="text-comp-2">Intro</div>
+            <div class="LevelName text-comp-2">Intro</div>
     </div>
     <div class="flex w-full h-1/6 p-1 items-center justify-center font-semibold">
         <button onclick="location.href='/level'" class="w-full text-center bg-comp-1 text-color rounded p-px hover:opacity-80 font-bold">
